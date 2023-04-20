1d5e738c87e6ef4e2cbb2546aec5ba82b248aaed
Xilin Li <lixl1228@126.com>
2023-04-19 20:06:42 -0400

    init

--- smooth.py
+++ smooth.py
@@ -53,6 +53,41 @@ def modeling1(obstacles,original_data,distance_ratio):
     val = np.zeros((n,3))
     val[:,0:2] =  np.array(sol.value(Q))
     return val
+
+
+def modeling2(obstacles,original_data,distance_ratio):
+    # mo
+    n = len(original_data)
+    rl=0.1
+    ru = 1
+    r_array = np.linalg.norm(original_data[1:,0:2]-original_data[0:-1,0:2])
+    d = np.sum(r_array)/n
+        
+    centers,radius = BubbleGeneration(original_data,obstacles,rl,ru)
+    opti = ca.Opti()
+    Q = opti.variable(n,2)
+    D = Q[1:,:]-Q[0:-1,:]
+    DD = D[1:,:]-D[0:-1,:]
+    opti.minimize(ca.dot(DD,DD))
+    sst_dm=ca.DM(original_data)
+    opti.subject_to(Q[0,:]==sst_dm[0,0:2])
+    # opti.subject_to(Q[1,:]==Q[0,:]+d*ca.DM([[ca.cos(original_data[0,2]),ca.sin(original_data[0,2])]]))
+    opti.subject_to(Q[-1,:]==sst_dm[-1,0:2])
+    # opti.subject_to(Q[1,:]==sst[0,0:2]+d*ca.tan(sst[0,2]))
+    diff = (Q-centers)*(Q-centers)
+    opti.subject_to(ca.sum2(diff)<=radius*radius)
+    a = ca.DM([3])
+    opti.solver('ipopt')
+    sol = opti.solve()
+    Q = np.array(sol.value(Q))
+    
+    
+    displace = np.zeros((n,3))
+    displace[1:,0:2] = (Q[1:,:]-Q[0:-1,:])*distance_ratio
+    
+    
+    
+    return Q,displace
     
 def BubbleGeneration(sst,obstacles,rl,ru):
     ces_obstacle = Obstacle(obstacles)
@@ -216,7 +251,7 @@ def process(sst_data,obstacles,E,A,Iz,rho,distance_ratio=0.9,total_iters=500,dt_
     obs_time_elasped = 0.0
     
     positions_cp = cp.array(positions)
-    sst_data_cp = cp.array(sst_data)
+    sst_data_cp = cp.array(positions)
     
     displacement = cp.zeros((node_count,3),dtype=cp.float32)
     displacement[0,2] = theta0
@@ -290,9 +325,223 @@ def process(sst_data,obstacles,E,A,Iz,rho,distance_ratio=0.9,total_iters=500,dt_
     print('obstacle force time: ',obs_time_elasped)
     return positions,displacement_history
 
-            
-if __name__ == "__main__":
+
+def process1(sst_data,obstacles,E,A,Iz,rho,distance_ratio=0.9,total_iters=500,dt_per_it=0.2,dt=0.0005,obstacle_force_ratio=500):
     
+    sst_data = np.array(sst_data,dtype=np.float32) 
+    
+    obstacle_force = ObstacleForce(obstacles)    
+    node_count = len(sst_data)
+    element_count = node_count-1
+    
+    constraint_index = [0,1,2,3*element_count,3*element_count+1]
+    n_dof = 3*node_count-5
+    total_dof = 3*node_count
+
+    internal_force = 1e0
+    
+    
+    # sst_distance = np.linalg.norm(sst_data[:,0:2]-sst_data[0,0:2],axis=1)*distance_ratio
+    # positions = np.zeros((node_count,3),dtype=np.float32)
+    # positions[:,0] = sst_distance*np.cos(phi0)
+    # positions[:,1] = sst_distance*np.sin(phi0)        
+    # positions = positions + sst_data[0,:]
+    # positions[0,2] = phi0
+    
+    
+    idx1 = np.zeros(3*node_count,dtype=int)
+    idx1[0:node_count]=np.arange(0,3*node_count,3)
+    idx1[node_count:2*node_count]=np.arange(1,3*node_count+1,3)
+    idx1[2*node_count:3*node_count]=np.arange(2,3*node_count+1,3)
+    
+    M = np.zeros((total_dof,total_dof),dtype=np.float32)
+    K = np.zeros((total_dof,total_dof),dtype=np.float32)
+    
+    K1 = np.zeros((total_dof,total_dof),dtype=np.float32)
+    K2 = np.zeros((total_dof,total_dof),dtype=np.float32)
+    # yp = np.zeros((2*node_count+1,),dtype=np.float32)
+    
+    # yp_cp = cp.zeros((2*node_count+1,),dtype=cp.float32)
+    
+    current_positions = modeling1(obstacles,sst_data,distance_ratio)
+    
+    original_displacement = np.zeros((node_count,3),dtype=cp.float32)
+    original_displacement[1:,] = (current_positions[1:,]-current_positions[0:-1,:])*distance_ratio
+    # original_positions[1:,0:2] = current_positions[0:-1,0:2] + (current_positions[1:,0:2]-current_positions[0:-1,0:2])*distance_ratio
+
+    
+    theta0 = sst_data[0,2]-np.arctan2(current_positions[1,1]-current_positions[0,1],current_positions[1,0]-current_positions[0,0])
+    if theta0>np.pi:
+        theta0-=2*np.pi
+    
+    node_list = [Node(i,pos[0:2]) for (i,pos) in enumerate(current_positions)]
+    element_list = [Element(i,node_list[i],node_list[i+1],E[i],A,Iz,rho) for i in range(element_count)]
+    
+    
+    for i,element in enumerate(element_list):
+        i1 = 3*node_list.index(element.node1)
+        i2 = 3*node_list.index(element.node2)
+        k_e = element.global_stiffness_matrix()
+        m_e = element.global_mass_matrix()
+        
+        K[i1:i1+3,i1:i1+3] += k_e[0:3,0:3]
+        K[i1:i1+3,i2:i2+3] += k_e[0:3,3:6]
+        K[i2:i2+3,i1:i1+3] += k_e[3:6,0:3]
+        K[i2:i2+3,i2:i2+3] += k_e[3:6,3:6]
+
+        M[i1:i1+3,i1:i1+3] += m_e[0:3,0:3]
+        M[i1:i1+3,i2:i2+3] += m_e[0:3,3:6]
+        M[i2:i2+3,i1:i1+3] += m_e[3:6,0:3]
+        M[i2:i2+3,i2:i2+3] += m_e[3:6,3:6]
+    
+    
+    K1[0:node_count,:]=K[idx1[0:node_count],:]
+    K1[node_count:2*node_count,:]=K[idx1[node_count:2*node_count],:]
+    K1[2*node_count:,:]=K[idx1[2*node_count:],:]
+    
+    
+    K2[:,0:node_count]=K1[:,idx1[0:node_count]]
+    K2[:,node_count:2*node_count]=K1[:,idx1[node_count:2*node_count]]
+    K2[:,2*node_count:]=K1[:,idx1[2*node_count:]]
+    
+    K2_cp = cp.array(K2,dtype=cp.float32)
+    
+    
+    displacement_history =[]
+
+    
+
+    constraint_deform = np.zeros((3*node_count,),dtype=np.float32)
+    constraint_deform[2]=theta0
+    # constraint_deform[0:2] = sst_data[0,0:2]-positions[0,0:2]
+    # constraint_deform[-3:-1] = sst_data[-1,0:2]-positions[-1,0:2]
+
+    # print(constraint_deform[-6:])
+    F_x = np.zeros((total_dof,))
+    F_xe=[internal_force,0,0,-internal_force,0,0]
+    for i in range(node_count-1):
+        F_x[3*i:3*i+6] =  F_x[3*i:3*i+6]+np.matmul(element_list[i].T,F_xe)
+    
+    F_d = np.matmul(K,constraint_deform)
+    
+    M_del = np.delete(M,constraint_index,axis=0)
+    M_del = np.delete(M_del,constraint_index,axis=1)
+    K_del = np.delete(K,constraint_index,axis=0)
+    K_del = np.delete(K_del,constraint_index,axis=1)
+    F_d_del = np.delete(F_d,constraint_index)
+    F_x_del = np.delete(F_x,constraint_index)
+    # print(M_del)
+        
+    M_inv = np.linalg.inv(M_del)
+    M_inv_root = fractional_matrix_power(M_del,-0.5)  
+    C = np.matmul(np.matmul(M_inv_root,K_del),M_inv_root)          
+    C = 2*fractional_matrix_power(C,0.5) 
+    C.real[abs(C.real)<1e-5]=0.0
+    C = np.matrix(C.real,dtype=np.float32)
+
+    # M_cp = cp.array(M_del)
+    K_cp = cp.array(K_del)
+    M_inv_cp = cp.array(M_inv)
+    C_cp = cp.array(C)
+    F_d_cp = cp.array(F_d_del,dtype=cp.float32)
+    F_x_cp = cp.array(F_x_del,dtype=cp.float32)
+    
+    
+    # print(p)
+    
+      
+    its = int(dt_per_it/dt)
+    
+    integrator_time_elasped = 0.0
+    obs_time_elasped = 0.0
+    
+    positions_cp = cp.array(current_positions-original_displacement)
+    sst_data_cp = cp.array(current_positions)
+    
+    displacement =cp.array(original_displacement)
+    # displacement = cp.zeros((node_count,3),dtype=cp.float32)
+    displacement[0,2] = theta0
+    # displacement[:,0:2] = sst_data_cp[:,0:2]-positions_cp[:,0:2]
+    
+
+    # displacement[1:,0:2] = positions_cp[0:-1,0:2] + (positions_cp[1:,0:2]-positions_cp[0:-1,0:2])*distance_ratio
+    # displacement[:,2]=positions_cp[:2,]
+    
+    
+    y0 =cp.reshape(displacement,(-1,))
+    
+    y1_cp = cp.zeros((2*n_dof,))
+    y1_cp[0:n_dof-1] = y0[3:3*(node_count-1)]
+    y1_cp[n_dof-1] = y0[3*(node_count-1)+2]
+        
+    
+    for i in range(total_iters):
+        print('{}/{}'.format(i,total_iters))
+        
+        # node_list = [Node(i,pos[0:2]) for (i,pos) in enumerate(positions)]
+        # element_list = [Element(i,node_list[i],node_list[i+1],E[i],A,Iz,rho) for i in range(element_count)]
+        
+        start = time.time()
+        
+        obs_force = obstacle_force.get_force(cp.asnumpy(sst_data_cp),k=obstacle_force_ratio)
+        obs_force = np.delete(obs_force,constraint_index)
+        obs_force_cp = cp.array(obs_force,dtype=cp.float32).reshape((-1,))        
+        end = time.time()
+        
+        obs_time_elasped+=(end-start)
+        #F = obs_force_cp-F_d_cp
+        F = obs_force_cp-F_d_cp + F_x_cp
+        
+        def model(y):
+            y_dot = cp.zeros_like(y)
+            y_dot[0:n_dof]=y[n_dof:]
+            temp = F-cp.matmul(C_cp,y[n_dof:])-cp.matmul(K_cp,y[0:n_dof])
+            y_dot[n_dof:] = cp.matmul(M_inv_cp,temp)
+            return y_dot 
+        
+        
+              
+        
+        
+        # yp_cp[0:node_count] = displacement[:,0]
+        # yp_cp[node_count:2*node_count] = displacement[:,1]
+        # yp_cp[2*node_count] = displacement[0,2]
+        
+        # fp = cp.matmul(K2_cp[2*node_count+1:,0:2*node_count+1],yp_cp)
+        # theta = cp.linalg.solve(K2_cp[2*node_count+1:,2*node_count+1:],fp)
+        # displacement[1:,2]=theta    
+        
+       
+        y1_cp[n_dof:]=0.0
+        start = time.time()
+        for j in range(its):
+            k1 = model(y1_cp) 
+            k2 = model(y1_cp+dt/2*k1)
+            k3 = model(y1_cp+dt/2*k2)
+            k4 = model(y1_cp+dt*k3)
+            y1_cp = y1_cp + dt/6*(k1+2*k2+2*k3+k4)
+        end = time.time()
+        integrator_time_elasped+=(end-start)
+        # print(y1_cp)        
+        # yt = cp.asnumpy(y1_cp[0:n_dof])
+
+        # for c in constraint_index:
+        #     yt = np.insert(yt,[c],y0[c])
+        
+        y0[3:3*(node_count-1)]=y1_cp[0:n_dof-1]
+        y0[3*(node_count-1)+2]=y1_cp[n_dof-1]
+        
+        yt = cp.reshape(y0,(-1,3))
+        displacement_history.append(cp.asnumpy(yt)) 
+        
+        sst_data_cp = positions_cp+yt
+
+    print('integration time: ',integrator_time_elasped)
+    print('obstacle force time: ',obs_time_elasped)
+    return current_positions-original_displacement,displacement_history
+
+
+def test():
     script_dir = os.path.dirname(__file__)
     
     # read obstacle file
@@ -324,7 +573,7 @@ if __name__ == "__main__":
     E=1e7*np.linspace(1,1,len(sst_state)-1)
     # E[0:10] = E[0]
     rho=20000
-    total_iters=40
+    total_iters=10
     dt_per_it=0.2
     dt=0.0001
     
@@ -333,7 +582,7 @@ if __name__ == "__main__":
     phi0 = sst_state[0][2]
     
     time_start = time.time()
-    positions,displacement_history = process(sst_state,obstacles,E,A,Iz,rho,distance_ratio=distance_ratio,total_iters=total_iters,dt_per_it=dt_per_it,dt=dt,obstacle_force_ratio=obstacle_force_ratio)
+    positions,displacement_history = process1(sst_state,obstacles,E,A,Iz,rho,distance_ratio=distance_ratio,total_iters=total_iters,dt_per_it=dt_per_it,dt=dt,obstacle_force_ratio=obstacle_force_ratio)
     time_end = time.time()  
     print('process time:',time_end-time_start)  
     
@@ -363,6 +612,158 @@ if __name__ == "__main__":
 
     plt.legend()
     plt.show()
+
+def test_fx():
+    script_dir = os.path.dirname(__file__)
+    
+    # read obstacle file
+    obstacles=[]
+    obstacle_file = os.path.join(script_dir, 'data/fem_refine/extended_polygon.shp')
+    with fiona.open(obstacle_file) as shapefile:
+        for record in shapefile:
+            geometry = shape(record['geometry'])
+            x5,y5 = geometry.exterior.xy
+            obstacles.append(np.vstack([x5,y5]).transpose())
+    
+    # read sst file    
+    sst_file = os.path.join(script_dir, 'data/fem_refine/sst_data.txt')
+    sst_state = np.array(ca.DM.from_file(sst_file))
+    if len(sst_state)%2 == 1:
+        sst_state=sst_state[0::2,:]
+    else:
+        sst_state1 = np.zeros((len(sst_state)/2+1,len(sst_state[0,:])))
+        sst_state1[0:-1,:] = sst_state[0::2,:]
+        sst_state1[-1,:] = sst_state[-1,:]
+        sst_state = sst_state1
+    
+    
+    
+    
+    r=0.3
+    A=2*np.pi*r*r
+    Iz =10*np.pi*r*r*r*r
+    E=1e8*np.linspace(1,1,len(sst_state)-1)
+    # E[0:10] = E[0]
+    rho=20000
+    total_iters=10
+    dt_per_it=0.4
+    dt=0.0001
+    
+    distance_ratio = 0.9
+    obstacle_force_ratio = 1e4
+    phi0 = sst_state[0][2]
+    
+    time_start = time.time()
+    positions,displacement_history = process1(sst_state,obstacles,E,A,Iz,rho,distance_ratio=distance_ratio,total_iters=total_iters,dt_per_it=dt_per_it,dt=dt,obstacle_force_ratio=obstacle_force_ratio)
+    time_end = time.time()  
+    print('process time:',time_end-time_start)  
+    
+    # positions = np.zeros((node_count,3),dtype=np.float32)
+    # positions[:,0] = sst_distance*np.cos(phi0)
+    # positions[:,1] = sst_distance*np.sin(phi0)        
+    # positions = positions + sst_state[0,:]
+    # positions[0,2] = phi0
+    theta0 = sst_state[0,2]-np.arctan2(sst_state[-1,1]-sst_state[0,1],sst_state[-1,0]-sst_state[0,0])
+    if theta0>np.pi:
+        theta0-=2*np.pi
+    
+    ax = plt.subplot(1,1,1)
+    obstacle_force = ObstacleForce(obstacles)
+    obstacle_force.plot_obstacles(ax)
+    # ax.plot(sst_state[0:3,0],sst_state[0:3,1],label='{}'.format(0))
+    ax.plot(sst_state[:,0],sst_state[:,1],label='{}'.format(-1))
+    ax.plot(positions[:,0],positions[:,1],label='{}'.format(0))
+    for i,d in enumerate(displacement_history):
+        if (i+1)%5==0:            
+            ax.plot(d[:,0]+positions[:,0],d[:,1]+positions[:,1],label='{}'.format(i+1))
+            # ax.plot(d[0:3,0]+positions[0:3,0],d[0:3,1]+positions[0:3,1],label='{}'.format(i+1))
+            # ax.arrow(d[1,0]+positions[1,0],d[1,1]+positions[1,1],np.cos(d[1,2]-theta0),np.sin(d[1,2]-theta0))
+            # ax.arrow(d[2,0]+positions[2,0],d[2,1]+positions[2,1],np.cos(d[2,2]-theta0),np.sin(d[2,2]-theta0))
+            print('theta',d[:,2])
+    ax.arrow(0,0,np.cos(sst_state[0,2]),np.sin(sst_state[0,2]),width=0.1,label='initial direction')
+
+    plt.legend()
+    plt.show()
+
+def compare_fx():
+    script_dir = os.path.dirname(__file__)
+    
+    # read obstacle file
+    obstacles=[]
+    obstacle_file = os.path.join(script_dir, 'data/fem_refine/extended_polygon.shp')
+    with fiona.open(obstacle_file) as shapefile:
+        for record in shapefile:
+            geometry = shape(record['geometry'])
+            x5,y5 = geometry.exterior.xy
+            obstacles.append(np.vstack([x5,y5]).transpose())
+    
+    # read sst file    
+    sst_file = os.path.join(script_dir, 'data/fem_refine/sst_data.txt')
+    sst_state = np.array(ca.DM.from_file(sst_file))
+    if len(sst_state)%2 == 1:
+        sst_state=sst_state[0::2,:]
+    else:
+        sst_state1 = np.zeros((len(sst_state)/2+1,len(sst_state[0,:])))
+        sst_state1[0:-1,:] = sst_state[0::2,:]
+        sst_state1[-1,:] = sst_state[-1,:]
+        sst_state = sst_state1
+    
+    
+    
+    
+    r=0.3
+    A=5*np.pi*r*r
+    Iz =10*np.pi*r*r*r*r
+    E=1e8*np.linspace(1,1,len(sst_state)-1)
+    E[0:3] = E[0]*10
+    rho=20000
+    total_iters=5
+    dt_per_it=0.4
+    dt=0.0001
+    
+    distance_ratio = 0.5
+    obstacle_force_ratio = 1e4
+    phi0 = sst_state[0][2]
+    
+    time_start = time.time()
+    positions,displacement_history = process(sst_state,obstacles,E,A,Iz,rho,distance_ratio=distance_ratio,total_iters=total_iters,dt_per_it=dt_per_it,dt=dt,obstacle_force_ratio=obstacle_force_ratio)
+    positions1,displacement_history1 = process1(sst_state,obstacles,E,A,Iz,rho,distance_ratio=distance_ratio,total_iters=total_iters,dt_per_it=dt_per_it,dt=dt,obstacle_force_ratio=obstacle_force_ratio)
+    time_end = time.time()  
+    print('process time:',time_end-time_start)  
+    
+    # positions = np.zeros((node_count,3),dtype=np.float32)
+    # positions[:,0] = sst_distance*np.cos(phi0)
+    # positions[:,1] = sst_distance*np.sin(phi0)        
+    # positions = positions + sst_state[0,:]
+    # positions[0,2] = phi0
+    theta0 = sst_state[0,2]-np.arctan2(sst_state[-1,1]-sst_state[0,1],sst_state[-1,0]-sst_state[0,0])
+    if theta0>np.pi:
+        theta0-=2*np.pi
+    
+    ax = plt.subplot(1,1,1)
+    obstacle_force = ObstacleForce(obstacles)
+    obstacle_force.plot_obstacles(ax)
+    # ax.plot(sst_state[0:3,0],sst_state[0:3,1],label='{}'.format(0))
+    ax.plot(sst_state[:,0],sst_state[:,1],label='{}'.format(-1))
+    ax.plot(positions[:,0],positions[:,1],label='{}'.format(0))
+    
+    ax.plot(displacement_history[-1][:,0]+positions[:,0],displacement_history[-1][:,1]+positions[:,1],label='Without Fx')
+    ax.plot(displacement_history1[-1][:,0]+positions1[:,0],displacement_history1[-1][:,1]+positions1[:,1],label='With Fx')
+    # for i,d in enumerate(displacement_history):
+    #     if (i+1)%5==0:            
+    #         ax.plot(d[:,0]+positions[:,0],d[:,1]+positions[:,1],label='{}'.format(i+1))
+    #         # ax.plot(d[0:3,0]+positions[0:3,0],d[0:3,1]+positions[0:3,1],label='{}'.format(i+1))
+    #         # ax.arrow(d[1,0]+positions[1,0],d[1,1]+positions[1,1],np.cos(d[1,2]-theta0),np.sin(d[1,2]-theta0))
+    #         # ax.arrow(d[2,0]+positions[2,0],d[2,1]+positions[2,1],np.cos(d[2,2]-theta0),np.sin(d[2,2]-theta0))
+    #         print('theta',d[:,2])
+    ax.arrow(0,0,np.cos(sst_state[0,2]),np.sin(sst_state[0,2]),width=0.1,label='initial direction')
+
+    plt.legend()
+    plt.show()
+            
+if __name__ == "__main__":
+    #test_fx()
+    compare_fx()
     
     
     
